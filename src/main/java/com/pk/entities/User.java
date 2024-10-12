deb007f1c06628aa5ea5a227f2bc1e3504e36f3b
Pratham Kumar <prk61dlw@gmail.com>
2024-10-12 14:42:54 +0530

    added user table with hibernate annotations

---
+++ src/main/java/com/pk/entities/User.java
@@ -0,0 +1,79 @@
+package com.pk.entities;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class User {
+
+    // while using hibernate we must add a unique entity
+    // @Id annotation for primary key
+    @Id
+
+    // @Column annotation for all columns
+    @Column
+    private long id;
+    @Column
+    private String name;
+    @Column
+    private String email;
+    @Column
+    private String password;
+    @Column
+    private String gender;
+    @Column
+    private String city;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/pk/main/App.java
@@ -0,0 +1,13 @@
+package com.pk.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
